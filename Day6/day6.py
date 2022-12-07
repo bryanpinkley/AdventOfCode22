39f46f634c1a9f83f97595a86a3d910934afea95
Bryan <bryanpinkley5@gmail.com>
2022-12-06 23:12:04 -0600

    Day 6 - Part 2

--- Day6/day6.py
+++ Day6/day6.py
@@ -8,8 +8,17 @@ for line in file:
         marker = line[counter] + line[counter+1] + line[counter+2] + line[counter+3]
         freq = Counter(marker)
         if len(freq) == 4:
-            print(marker)
             print(counter+4)
             break
         counter += 1
 
+    counter = 0
+    while counter < len(line):
+        marker = ""
+        for i in range(14):
+            marker += line[counter+i]
+        freq = Counter(marker)
+        if len(freq) == 14:
+            print(counter+14)
+            break
+        counter += 1
